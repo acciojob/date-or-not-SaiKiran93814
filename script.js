ebf844db83148c38165b943df2664f7dd61a1391
SaiKiran93814 <60767499+SaiKiran93814@users.noreply.github.com>
2025-05-13 11:36:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ return (input instanceof Date && !isNaN(input)) ||
 };
 
 // Do not change the code below.
-//const input = prompt("Enter Date.");
+const input = prompt("Enter Date.");
 alert(isDate(input));
